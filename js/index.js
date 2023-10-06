c8ad535aa466635a5c41bceac80888366aebeb46
Peter Ayodeji <pb.ayodeji@gmail.com>
2023-10-06 15:02:21 +0100

    build header section and make mobile navigation work

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// # Make mobile navigation work
+
+const btnNavEl = document.querySelector(".nav__mobile");
+const navEl = document.querySelector(".nav");
+
+btnNavEl.addEventListener("click", function () {
+  navEl.classList.toggle("nav-open");
+
+  // document.body.classList.toggle("hide-overflow");
+});
