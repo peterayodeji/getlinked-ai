105a1f5797dcc69eba6a797ab13163b46d841f1c
Peter Ayodeji <pb.ayodeji@gmail.com>
2023-11-03 10:47:39 +0100

    implement smooth scrolling animation feature

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // # Sticky Navigation
 let isIntersecting = true;
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // # Make mobile navigation work
 const btnNavEl = document.querySelector(".nav__mobile");
 const navEl = document.querySelector(".nav");
@@ -6,4 +8,37 @@ btnNavEl.addEventListener("click", function () {
   navEl.classList.toggle("nav-open");
 
   // document.body.classList.toggle("hide-overflow");
+});
+
+// # Smooth scrolling animation
+// const navLinksContainer = document.querySelector(".nav__links");
+// const navLinks = navLinksContainer.querySelectorAll(".link");
+// console.log(navLinks);
+
+const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+
+    if (!href.startsWith("#")) return;
+    e.preventDefault();
+
+    // * Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // * Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
 });
